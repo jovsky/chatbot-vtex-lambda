91a6ab7e1ad9bf52233c90fe1433f32093a69d81
Rogerio Alcantara <carpe@carpetecnologia.com.br>
2020-09-12 15:06:18 -0300

    correções no method

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// export default axios.create({
-//   baseURL: "https://hiringcoders14.vtexcommercestable.com.br/api",
-//   methor: "GET",
-//   headers: {
-//     'accept': 'application/json',
-//     'content-type': 'application/json',
-//     'x-vtex-api-appkey': 'vtexappkey-hiringcoders14-PYVTSB',
-//     'x-vtex-api-apptoken': 'TLAPVRTTFHVDDIIXBTDIUWDILDKLUKAMFWDQVFNKIMRJMTIGYFRYBIEWIRDRHIZUHIXHFRC'
-//   }
-// });
+export default axios.create({
+  baseURL: "https://hiringcoders14.vtexcommercestable.com.br/api",
+  method: "GET",
+  headers: {
+    'accept': 'application/json',
+    'content-type': 'application/json',
+    'x-vtex-api-appkey': 'vtexappkey-hiringcoders14-PYVTSB',
+    'x-vtex-api-apptoken': 'TLAPVRTTFHVDDIIXBTDIUWDILDKLUKAMFWDQVFNKIMRJMTIGYFRYBIEWIRDRHIZUHIXHFRC'
+  }
+});
