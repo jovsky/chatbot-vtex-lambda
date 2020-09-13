f3ddb7790c3188c027fd4676de787847e20d88e4
JoaoVitor <jvmelop@gmail.com>
2020-09-13 14:50:19 -0300

    cards categoria e opcao

--- funcoes/sugerirProduto.js
+++ funcoes/sugerirProduto.js
@@ -24,71 +24,73 @@ const getCategorias = async () => {
   return result
 }
 
-
 async function validate(slots) {
 
+  const categoria = slots.categoria;
   
-  const { categoria, numero, cor } = slots;
-  
-  
-  const categorias = await getCategorias()
-  console.log(' Categorias da API:', categorias)
+  const categorias = await getCategorias();
+  // console.log(' Categorias da API:', categorias)
 
-  /* if (categoria !== null && !categorias.includes(categoria.toLowerCase())) {
-    //console.log(' >>> ', categorias, tipoRoupa.toLowerCase(), categorias.includes(tipoRoupa.toLowerCase()))
-    return {
-      isValid: false,
-      violatedSlot: "tipoRoupa",
-      message: {
-        contentType: "PlainText",
-        content: "Desculpe, não possuimos esta categoria na loja, temos por exemplo " + categorias[4]+ ". Qual deseja?"
-      }
-    }
-  }  */
-
-  if (categoria !== null && !categorias.includes(categoria.toLowerCase())) {
+  if (!categorias.includes(categoria.toLowerCase())) {
     return {
       isValid: false,
       violatedSlot: "categoria",
-      /* message: {
-        contentType: "PlainText",
-        content: `Desculpe, trabalhamos apenas com as categorias: ${categorias}. Qual deseja?`,
-      }, */
-      responseCard: {
-        version: 1,
-        contentType: "application/vnd.amazonaws.card.generic",
-        genericAttachments: [
-          {
-            title: `Desculpe, trabalhamos apenas com as seguintes categorias. Qual deseja?`,
-            subTitle: "Escolha uma das opcoes", 
-            imageUrl: "https://hiringcoders14.vtexassets.com/assets/vtex/assets-builder/vtex.minimumtheme/0.1.0/jaquetaAzul___a3a35d6a629d8738bd26dc2628486c69.jpg",
-            buttons: [
-              {
-                text: categorias[0],
-                value: "blusas"
-              },
-              {
-                text: categorias[1],
-                value: "chapeus"
-              },
-              {
-                text: categorias[2],
-                value: "camisas"
-              },
-              {
-                text: categorias[3],
-                value: "camisas"
-              },
-              {
-                text: categorias[4],
-                value: "camisas"
-              }
-            ]
-          }
-        ]
-      }
+      // message: {
+      //   contentType: "PlainText",
+      //   content: "Desculpe, não possuimos esta categoria na loja. Escolha uma das categorias abaixo:"
+      // },
+      responseCard: await cardCategorias()
     }
-  } /* else if(categoria !== null && categorias.includes(categoria.toLowerCase())){
+  } 
+
+  return {
+    isValid: true
+  }
+
+  // if (categoria !== null && !categorias.includes(categoria.toLowerCase())) {
+  //   return {
+  //     isValid: false,
+  //     violatedSlot: "categoria",
+  //     /* message: {
+  //       contentType: "PlainText",
+  //       content: `Desculpe, trabalhamos apenas com as categorias: ${categorias}. Qual deseja?`,
+  //     }, */
+  //     responseCard: {
+  //       version: 1,
+  //       contentType: "application/vnd.amazonaws.card.generic",
+  //       genericAttachments: [
+  //         {
+  //           title: `Desculpe, trabalhamos apenas com as seguintes categorias. Qual deseja?`,
+  //           subTitle: "Escolha uma das opcoes", 
+  //           imageUrl: "https://hiringcoders14.vtexassets.com/assets/vtex/assets-builder/vtex.minimumtheme/0.1.0/jaquetaAzul___a3a35d6a629d8738bd26dc2628486c69.jpg",
+  //           buttons: [
+  //             {
+  //               text: categorias[0],
+  //               value: "blusas"
+  //             },
+  //             {
+  //               text: categorias[1],
+  //               value: "chapeus"
+  //             },
+  //             {
+  //               text: categorias[2],
+  //               value: "camisas"
+  //             },
+  //             {
+  //               text: categorias[3],
+  //               value: "camisas"
+  //             },
+  //             {
+  //               text: categorias[4],
+  //               value: "camisas"
+  //             }
+  //           ]
+  //         }
+  //       ]
+  //     }
+  //   }
+  // } 
+  /* else if(categoria !== null && categorias.includes(categoria.toLowerCase())){
     return {
       isValid: true,
       currentSlot: "categoria",
@@ -102,47 +104,47 @@ async function validate(slots) {
     }
   }  */
 
-  if (numero !== null) {
-    // console.log(" TERCEIRO LOG:", numero, numero.toLowerCase(), CONTAINER_NUMERO_PMG.includes(numero.toLowerCase()), CONTAINER_NUMERO_PMG.includes(numero.toLowerCase()) )
-    if (categorias.includes(categoria.toLowerCase())) {
-      if (!CONTAINER_NUMERO_PMG.includes(numero.toLowerCase())) {
-        return {
-          isValid: false,
-          violatedSlot: "numero",
-          message: {
-            contentType: "PlainText",
-            content: "Não possuímos este tamanho para este item. Temos disponíveis PP, P, M, G, XG. Qual deseja?"
-          }
-        }
-      }
-    } else {
-      if (!CONTAINER_NUMERO_NUM.includes(numero.toLowerCase())) {
-        return {
-          isValid: false,
-          violatedSlot: "numero",
-          message: {
-            contentType: "PlainText",
-            content: "Não possuímos este tamanho para este item. Escolha um tamanho entre 35 e 46."
-          }
-        }
-      }
-    }
-  }
+  // if (numero !== null) {
+  //   // console.log(" TERCEIRO LOG:", numero, numero.toLowerCase(), CONTAINER_NUMERO_PMG.includes(numero.toLowerCase()), CONTAINER_NUMERO_PMG.includes(numero.toLowerCase()) )
+  //   if (categorias.includes(categoria.toLowerCase())) {
+  //     if (!CONTAINER_NUMERO_PMG.includes(numero.toLowerCase())) {
+  //       return {
+  //         isValid: false,
+  //         violatedSlot: "numero",
+  //         message: {
+  //           contentType: "PlainText",
+  //           content: "Não possuímos este tamanho para este item. Temos disponíveis PP, P, M, G, XG. Qual deseja?"
+  //         }
+  //       }
+  //     }
+  //   } else {
+  //     if (!CONTAINER_NUMERO_NUM.includes(numero.toLowerCase())) {
+  //       return {
+  //         isValid: false,
+  //         violatedSlot: "numero",
+  //         message: {
+  //           contentType: "PlainText",
+  //           content: "Não possuímos este tamanho para este item. Escolha um tamanho entre 35 e 46."
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
-  if (cor !== null && !CONTAINER_COR.includes(cor.toLowerCase())) {
-    return {
-      isValid: false,
-      violatedSlot: "cor",
-      message: {
-        contentType: "PlainText",
-        content: "Não possuímos este item nesta cor, escolha outra."
-      }
-    }
-  }
+  // if (cor !== null && !CONTAINER_COR.includes(cor.toLowerCase())) {
+  //   return {
+  //     isValid: false,
+  //     violatedSlot: "cor",
+  //     message: {
+  //       contentType: "PlainText",
+  //       content: "Não possuímos este item nesta cor, escolha outra."
+  //     }
+  //   }
+  // }
 
-  return {
-    isValid: true
-  }
+  // return {
+  //   isValid: true
+  // }
 }
 
 async function cardOpcao(categoria) {
@@ -163,27 +165,69 @@ async function cardOpcao(categoria) {
   }
 }
 
+async function cardCategorias() {
+
+  // const { categoria, numero, cor } = slots;
+  
+  const categorias = await getCategorias()
+  console.log(' Categorias da API:', categorias)
+
+  getCategorias
+
+  return {
+    isValid: false,
+    violatedSlot: "categoria",
+    /* message: {
+      contentType: "PlainText",
+      content: `Desculpe, trabalhamos apenas com as categorias: ${categorias}. Qual deseja?`,
+    }, */
+    responseCard: {
+      version: 1,
+      contentType: "application/vnd.amazonaws.card.generic",
+      genericAttachments: [
+        {
+          title: `Desculpe, trabalhamos apenas com as seguintes categorias. Qual deseja?`,
+          subTitle: "Escolha uma das opcoes", 
+          imageUrl: "https://hiringcoders14.vtexassets.com/assets/vtex/assets-builder/vtex.minimumtheme/0.1.0/jaquetaAzul___a3a35d6a629d8738bd26dc2628486c69.jpg",
+          buttons: [
+            {
+              text: categorias[0],
+              value: "blusas"
+            },
+            {
+              text: categorias[1],
+              value: "chapeus"
+            },
+            {
+              text: categorias[2],
+              value: "camisas"
+            },
+            {
+              text: categorias[3],
+              value: "camisas"
+            },
+            {
+              text: categorias[4],
+              value: "camisas"
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+
 async function dispatch(intentRequest, callback) {
+
+  let categoriaValida = false;
   
   const slots = intentRequest.currentIntent.slots;
 
-  // verificar se a categoria ainda precisa ser validada
-  if (true) {
-    
-    const resultValidation = await validate(slots);
+  // a categoria ainda nao foi informada
+  if (slots.categorias === null && slots.opcao === null) {
 
-  }
-  // categoria já está ok, agora vamos mandar o response Card com as opcoes da API
-  else if (true) {
+    const card = await cardCategorias()
 
-    const cardOpcao = await cardOpcao();
-
-  }
-
-
-  // se algum slot está invalido
-  if (!resultValidation.isValid) {
-    slots[resultValidation.violatedSlot] = null;
     callback({
       sessionAttributes: intentRequest.sessionAttributes,
       dialogAction: {
@@ -191,13 +235,79 @@ async function dispatch(intentRequest, callback) {
         intentName: intentRequest.currentIntent.name,
         slots,
         slotToElicit: resultValidation.violatedSlot,
-        responseCard: resultValidation.responseCard
+        responseCard: card
       }
     })
     return
+
+  }
+
+  // verificar se a categoria ainda precisa ser validada
+  else if (slots.categorias !== null && slots.opcao === null) {
+    
+    const resultValidation = await validate(slots);
+
+    // se a categoria for ivnalida
+    if (!resultValidation.isValid) {
+      slots['categoria'] = null;
+      callback({
+        sessionAttributes: intentRequest.sessionAttributes,
+        dialogAction: {
+          type: 'ElicitSlot',
+          intentName: intentRequest.currentIntent.name,
+          slots,
+          slotToElicit: 'categoria',
+          responseCard: resultValidation.responseCard
+        }
+      })
+      return
+    }
+    // se a cateogira está certa
+    else {
+      categoriaValida = true;
+    }
+
   }
   
-  // todos slots estão válidos
+  // categoria já está ok, agora vamos mandar o response Card com as opcoes da API
+  if (categoriaValida) {
+
+    const card = await cardOpcao(slots.categoria);
+
+    callback({
+      sessionAttributes: intentRequest.sessionAttributes,
+      dialogAction: {
+        type: 'ElicitSlot',
+        intentName: intentRequest.currentIntent.name,
+        slots,
+        slotToElicit: 'opcao',
+        responseCard: card
+      }
+    })
+    return
+
+  }
+
+
+  // // se algum slot está invalido
+  // if (!resultValidation.isValid) {
+  //   slots[resultValidation.violatedSlot] = null;
+  //   callback({
+  //     sessionAttributes: intentRequest.sessionAttributes,
+  //     dialogAction: {
+  //       type: 'ElicitSlot',
+  //       intentName: intentRequest.currentIntent.name,
+  //       slots,
+  //       slotToElicit: resultValidation.violatedSlot,
+  //       responseCard: resultValidation.responseCard
+  //     }
+  //   })
+  //   return
+  // }
+  
+  // // todos slots estão válidos
+
+
   callback({
     sessionAttributes: intentRequest.sessionAttributes,
     dialogAction: {
